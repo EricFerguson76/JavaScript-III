afe08b4e8d3541b8dbf14324d0dfef7d0f062bdb
EricFerguson76 <turtle0888@gmail.com>
2019-09-19 10:21:07 -0500

    Finished Project

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -15,15 +15,16 @@
   * dimensions (These represent the character's size in the video game)
   * destroy() // prototype method that returns: `${this.name} was removed from the game.`
 */
+
 function GameObject(thing) {
-  this.createdAt = thing.createdAt,
-    this.name = thing.name,
-    this.dimensions = thing.dimensions
+  this.createdAt = thing.createdAt;
+  this.name = thing.name;
+  this.dimensions = thing.dimensions;
 }
 
-GameObject.prototype.destroy = function () {
+GameObject.prototype.destroy = function() {
   return `${this.name} was removed from the game.`;
-}
+};
 /*
   === CharacterStats ===
   * healthPoints
@@ -32,17 +33,14 @@ GameObject.prototype.destroy = function () {
 */
 
 function CharacterStats(stats) {
-  this.healthPoints = stats.healthPoints
-  //GameObjects.call(this, stats);
+  GameObject.call(this, stats);
+  this.healthPoints = stats.healthPoints;
 }
 CharacterStats.prototype = Object.create(GameObject.prototype);
 
-
-CharacterStats.prototype.takeDamage = function () {
+CharacterStats.prototype.takeDamage = function() {
   return `${this.name} took damage`;
-}
-
-
+};
 
 /*
   === Humanoid (Having an appearance or character resembling that of a human.) ===
@@ -55,17 +53,16 @@ CharacterStats.prototype.takeDamage = function () {
 */
 
 function Humanoid(human) {
-  this.team = human.team,
-    this.weapons = human.weapons,
-    this.language = human.language
-  CharacterStats.call(this, human)
+  this.team = human.team;
+  this.weapons = human.weapons;
+  this.language = human.language;
+  CharacterStats.call(this, human);
 }
 Humanoid.prototype = Object.create(CharacterStats.prototype);
 
-Humanoid.prototype.greet = function () {
+Humanoid.prototype.greet = function() {
   return `${this.name} offers a greeting in ${this.language}`;
-}
-
+};
 
 /*
  * Inheritance chain: GameObject -> CharacterStats -> Humanoid
@@ -75,21 +72,18 @@ Humanoid.prototype.greet = function () {
 
 // Test you work by un-commenting these 3 objects and the list of console logs below:
 
-
 const mage = new Humanoid({
   createdAt: new Date(),
   dimensions: {
     length: 2,
     width: 1,
-    height: 1,
+    height: 1
   },
   healthPoints: 5,
   name: 'Bruce',
   team: 'Mage Guild',
-  weapons: [
-    'Staff of Shamalama',
-  ],
-  language: 'Common Tongue',
+  weapons: ['Staff of Shamalama'],
+  language: 'Common Tongue'
 });
 
 const swordsman = new Humanoid({
@@ -97,16 +91,13 @@ const swordsman = new Humanoid({
   dimensions: {
     length: 2,
     width: 2,
-    height: 2,
+    height: 2
   },
   healthPoints: 15,
   name: 'Sir Mustachio',
   team: 'The Round Table',
-  weapons: [
-    'Giant Sword',
-    'Shield',
-  ],
-  language: 'Common Tongue',
+  weapons: ['Giant Sword', 'Shield'],
+  language: 'Common Tongue'
 });
 
 const archer = new Humanoid({
@@ -114,16 +105,13 @@ const archer = new Humanoid({
   dimensions: {
     length: 1,
     width: 2,
-    height: 4,
+    height: 4
   },
   healthPoints: 10,
   name: 'Lilith',
   team: 'Forest Kingdom',
-  weapons: [
-    'Bow',
-    'Dagger',
-  ],
-  language: 'Elvish',
+  weapons: ['Bow', 'Dagger'],
+  language: 'Elvish'
 });
 
 console.log(mage.createdAt); // Today's date
@@ -137,8 +125,7 @@ console.log(archer.greet()); // Lilith offers a greeting in Elvish.
 console.log(mage.takeDamage()); // Bruce took damage.
 console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
 
-
-// Stretch task: 
-// * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
+// Stretch task:
+// * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.
 // * Give the Hero and Villains different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
 // * Create two new objects, one a villain and one a hero and fight it out with methods!
